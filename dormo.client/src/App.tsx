384c3069375356fcdaa258aaab5ebb0851b0fd5a
Azure DevOps <Mcjoseph.Agbanlog@wizard-ai.com>
2025-02-19 10:44:59 +0000

    Updated commit from McJoseph

--- dormo.client/src/App.tsx
+++ dormo.client/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
     return (
         <div>
             <h1 id="tableLabel">Weather forecast</h1>
-            <Button>Click me</Button>
+            <Button>Click fasfas</Button>
             <p>This component demonstrates fetching data from the server.</p>
             {contents}
         </div>
